017eb2e5ef769e0e6ff664480b7fccaae1e455e0
ZIDEN-3 <145407596+ZIDEN-3@users.noreply.github.com>
2023-10-20 04:08:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var menuToggle = document.querySelector('.menu-toggle');
+    var menu = document.querySelector('.menu');
+  
+    menuToggle.addEventListener('click', function() {
+      menu.classList.toggle('active');
+    });
+  });
